209d50892ebafe3e076c48e86552a2ff6b1d2c7c
jruaux <jruaux@gmail.com>
2023-12-04 22:35:14 -0800

    build: Upgraded to gradle 8.4

--- subprojects/redis-enterprise-admin/src/test/java/com/redis/enterprise/AdminTests.java
+++ subprojects/redis-enterprise-admin/src/test/java/com/redis/enterprise/AdminTests.java
@@ -4,6 +4,8 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.security.GeneralSecurityException;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -19,8 +21,6 @@ import org.junit.jupiter.api.TestInstance;
 import org.junit.jupiter.api.TestInstance.Lifecycle;
 import org.junit.jupiter.api.condition.EnabledOnOs;
 import org.junit.jupiter.api.condition.OS;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.util.unit.DataSize;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
@@ -37,98 +37,98 @@ import io.lettuce.core.cluster.api.StatefulRedisClusterConnection;
 @EnabledOnOs(value = OS.LINUX)
 class AdminTests {
 
-	private static final Logger log = LoggerFactory.getLogger(AdminTests.class);
+    private static final Logger log = Logger.getLogger(AdminTests.class.getName());
 
-	@Container
-	private static RedisEnterpriseContainer server = new RedisEnterpriseContainer(
-			RedisEnterpriseContainer.DEFAULT_IMAGE_NAME.withTag(RedisEnterpriseContainer.DEFAULT_TAG));
+    @Container
+    private static RedisEnterpriseContainer server = new RedisEnterpriseContainer(
+            RedisEnterpriseContainer.DEFAULT_IMAGE_NAME.withTag(RedisEnterpriseContainer.DEFAULT_TAG));
 
-	private static Admin admin;
+    private static Admin admin;
 
-	@BeforeAll
-	static void setupAdmin() throws ParseException, GeneralSecurityException, IOException {
-		admin = new Admin(RedisEnterpriseContainer.ADMIN_USERNAME,
-				RedisEnterpriseContainer.ADMIN_PASSWORD.toCharArray());
-		admin.setHost(server.getHost());
-	}
+    @BeforeAll
+    static void setupAdmin() throws ParseException, GeneralSecurityException, IOException {
+        admin = new Admin(RedisEnterpriseContainer.ADMIN_USERNAME, RedisEnterpriseContainer.ADMIN_PASSWORD.toCharArray());
+        admin.setHost(server.getHost());
+    }
 
-	@AfterAll
-	static void teardownAdmin() throws Exception {
-		admin.close();
-	}
+    @AfterAll
+    static void teardownAdmin() throws Exception {
+        admin.close();
+    }
 
-	@BeforeEach
-	void deleteAllDatabases() throws GeneralSecurityException, IOException, ParseException {
-		List<Database> databases = admin.getDatabases();
-		log.info("Deleting databases {}", databases.stream().map(Database::getUid).collect(Collectors.toList()));
-		for (Database database : databases) {
-			admin.deleteDatabase(database.getUid());
-		}
-		Awaitility.await().until(() -> admin.getDatabases().isEmpty());
-	}
+    @BeforeEach
+    void deleteAllDatabases() throws GeneralSecurityException, IOException, ParseException {
+        List<Database> databases = admin.getDatabases();
+        log.log(Level.INFO, "Deleting databases {0}", databases.stream().map(Database::getUid).collect(Collectors.toList()));
+        for (Database database : databases) {
+            admin.deleteDatabase(database.getUid());
+        }
+        Awaitility.await().until(() -> admin.getDatabases().isEmpty());
+    }
 
-	@Test
-	void createDatabase() throws ParseException, GeneralSecurityException, IOException {
-		String databaseName = "CreateDBTest";
-		admin.createDatabase(Database.name(databaseName).build());
-		Stream<Database> stream = admin.getDatabases().stream().filter(d -> d.getName().equals(databaseName));
-		Assertions.assertEquals(1, stream.count());
-	}
+    @Test
+    void createDatabase() throws ParseException, GeneralSecurityException, IOException {
+        String databaseName = "CreateDBTest";
+        admin.createDatabase(Database.name(databaseName).build());
+        Stream<Database> stream = admin.getDatabases().stream().filter(d -> d.getName().equals(databaseName));
+        Assertions.assertEquals(1, stream.count());
+    }
 
-	@Test
-	void createClusterDatabase() throws ParseException, GeneralSecurityException, IOException {
-		String databaseName = "CreateClusterDBTest";
-		admin.createDatabase(Database.name(databaseName).ossCluster(true)
-				.port(RedisEnterpriseContainer.DEFAULT_DATABASE_PORT).build());
-		List<Database> databases = admin.getDatabases();
-		Assertions.assertEquals(1, databases.size());
-		Assertions.assertEquals(databaseName, databases.get(0).getName());
-		Database database = databases.get(0);
-		RedisClusterClient client = RedisClusterClient.create(RedisURI.create(server.getHost(), database.getPort()));
-		try (StatefulRedisClusterConnection<String, String> connection = client.connect()) {
-			Assertions.assertEquals("PONG", connection.sync().ping());
-		}
-		client.shutdown();
-		client.getResources().shutdown();
-	}
+    @Test
+    void createClusterDatabase() throws ParseException, GeneralSecurityException, IOException {
+        String databaseName = "CreateClusterDBTest";
+        admin.createDatabase(
+                Database.name(databaseName).ossCluster(true).port(RedisEnterpriseContainer.DEFAULT_DATABASE_PORT).build());
+        List<Database> databases = admin.getDatabases();
+        Assertions.assertEquals(1, databases.size());
+        Assertions.assertEquals(databaseName, databases.get(0).getName());
+        Database database = databases.get(0);
+        RedisClusterClient client = RedisClusterClient.create(RedisURI.create(server.getHost(), database.getPort()));
+        try (StatefulRedisClusterConnection<String, String> connection = client.connect()) {
+            Assertions.assertEquals("PONG", connection.sync().ping());
+        }
+        client.shutdown();
+        client.getResources().shutdown();
+    }
 
-	@Test
-	void createSearchDatabase() throws ParseException, IOException {
-		String databaseName = "CreateSearchDBTest";
-		admin.createDatabase(Database.name(databaseName).module(RedisModule.SEARCH).build());
-		List<Database> databases = admin.getDatabases();
-		Assertions.assertEquals(1, databases.size());
-		Assertions.assertEquals(RedisModule.SEARCH.getModuleName(), databases.get(0).getModules().get(0).getName());
-	}
+    @Test
+    void createSearchDatabase() throws ParseException, IOException {
+        String databaseName = "CreateSearchDBTest";
+        admin.createDatabase(Database.name(databaseName).module(RedisModule.SEARCH).build());
+        List<Database> databases = admin.getDatabases();
+        Assertions.assertEquals(1, databases.size());
+        Assertions.assertEquals(RedisModule.SEARCH.getModuleName(), databases.get(0).getModules().get(0).getName());
+    }
 
-	@Test
-	void deleteDatabase() throws ParseException, GeneralSecurityException, IOException {
-		String databaseName = "DeleteDBTest";
-		Database database = admin.createDatabase(Database.name(databaseName).build());
-		admin.deleteDatabase(database.getUid());
-		Awaitility.await().until(() -> admin.getDatabases().stream().noneMatch(d -> d.getUid() == database.getUid()));
-	}
+    @Test
+    void deleteDatabase() throws ParseException, GeneralSecurityException, IOException {
+        String databaseName = "DeleteDBTest";
+        Database database = admin.createDatabase(Database.name(databaseName).build());
+        admin.deleteDatabase(database.getUid());
+        Awaitility.await().until(() -> admin.getDatabases().stream().noneMatch(d -> d.getUid() == database.getUid()));
+    }
 
-	@Test
-	void installModule() throws IOException, ParseException {
-		String gearsModuleFile = "redisgears.linux-bionic-x64.1.0.6.zip";
-		try (InputStream zipInputStream = getClass().getClassLoader().getResourceAsStream(gearsModuleFile)) {
-			log.info("Installing module {}", gearsModuleFile);
-			ModuleInstallResponse response = admin.installModule(gearsModuleFile, zipInputStream);
-			log.info("Installed module {}, action ID: {}", gearsModuleFile, response.getActionUid());
-			Assertions.assertTrue(
-					admin.getModules().stream().anyMatch(m -> m.getName().equals(RedisModule.GEARS.getModuleName())));
-		}
-		admin.createDatabase(Database.name("ModuleInstallDBTest").module(RedisModule.SEARCH).build());
-		List<Database> databases = admin.getDatabases();
-		Assertions.assertEquals(1, databases.size());
-		Assertions.assertEquals(RedisModule.SEARCH.getModuleName(), databases.get(0).getModules().get(0).getName());
-	}
+    @Test
+    void installModule() throws IOException, ParseException {
+        String gearsModuleFile = "redisgears.linux-bionic-x64.1.0.6.zip";
+        try (InputStream zipInputStream = getClass().getClassLoader().getResourceAsStream(gearsModuleFile)) {
+            log.log(Level.INFO, "Installing module {0}", gearsModuleFile);
+            ModuleInstallResponse response = admin.installModule(gearsModuleFile, zipInputStream);
+            log.log(Level.INFO, "Installed module {0}, action ID: {1}",
+                    new Object[] { gearsModuleFile, response.getActionUid() });
+            Assertions.assertTrue(
+                    admin.getModules().stream().anyMatch(m -> m.getName().equals(RedisModule.GEARS.getModuleName())));
+        }
+        admin.createDatabase(Database.name("ModuleInstallDBTest").module(RedisModule.SEARCH).build());
+        List<Database> databases = admin.getDatabases();
+        Assertions.assertEquals(1, databases.size());
+        Assertions.assertEquals(RedisModule.SEARCH.getModuleName(), databases.get(0).getModules().get(0).getName());
+    }
 
-	@Test
-	void createDatabaseException() throws ParseException, IOException {
-		Assertions.assertThrows(HttpResponseException.class, () -> admin.createDatabase(
-				Database.name("DatabaseCreateExceptionTestDB").memory(DataSize.ofGigabytes(10)).build()));
-	}
+    @Test
+    void createDatabaseException() throws ParseException, IOException {
+        Assertions.assertThrows(HttpResponseException.class, () -> admin
+                .createDatabase(Database.name("DatabaseCreateExceptionTestDB").memory(DataSize.ofGigabytes(10)).build()));
+    }
 
 }
